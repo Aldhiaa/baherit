be0c6e935b37df7621ccf88f1ae2a5a89f42cddd
BasheerSallam <58336467+BasheerSallam@users.noreply.github.com>
2026-01-04 20:58:47 +0300

    Update about.php

--- lang/en/about.php
+++ lang/en/about.php
@@ -3,7 +3,7 @@
 return [
     'tagline' => 'About Us',
     'title' => 'About Us',
-    'description' => 'At techin, we are dedicated to delivering innovative IT solutions and services that empower businesses to thrive in the digital age.',
+    'description' => 'At Baher Technology, we are dedicated to delivering innovative IT solutions and services that empower businesses to thrive in the digital age.',
     'service_1' => 'Providing Skillful Services',
     'service_2' => '24/7 Support For Clients',
     'more_info' => 'More Information',
